9d2fa15f54a873bf28c4658d6e48818c9e59e1b3
Emin Fedar <eminfedar@gmail.com>
2024-11-14 11:28:22 +0300

    remove website sources

--- gatsby-browser.js
+++
@@ -1,4 +0,0 @@
-import "./src/css/normalize.css"
-import './src/css/prism-okaidia.css'
-import './src/css/global.css'
-import './src/css/screen.css'
--- src/components/Tag.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
-const Tag = ({ tag }) =>
-    <Link className="tag-link" to={ `/tags/${ tag }` }>
-        { tag }
-    </Link>
-
-export default Tag
